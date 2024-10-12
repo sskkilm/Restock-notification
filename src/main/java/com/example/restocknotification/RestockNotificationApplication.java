4562f0c559f86fd0342d7195a92ac8afcb9207d3
sskkilm <sskkilm@naver.com>
2024-10-12 13:39:53 +0900

    Initial commit

---
+++ src/main/java/com/example/restocknotification/RestockNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.restocknotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestockNotificationApplication.class, args);
+    }
+
+}
